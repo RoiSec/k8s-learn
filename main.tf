9d26240e7248513ecd825bf4dc6ef39a4a0ce7b5
RoiSec <roi>
2021-11-06 17:04:16 +0200

    fixing issues

--- main.tf
+++ main.tf
@@ -9,12 +9,17 @@ module "roi-eks" {
   subnets         = module.roi-vpc.private_subnets
   cluster_endpoint_public_access_cidrs = var.allowing_ips
   cluster_iam_role_name = aws_iam_role.eks-cluster-role.name
+  workers_role_name = aws_iam_role.eks-node-group-role.name
+  cluster_endpoint_public_access = true // Allow networking from outside
+  cluster_endpoint_private_access = true //allow networking 
+  manage_worker_iam_resources = false
+  manage_cluster_iam_resources = false
   manage_aws_auth = var.manage_aws_auth
   cluster_tags = var.cluster_tags
   # node_groups = {
   #   roi-node-group = {
   #     desired_capacity = 3
-  #     max_capacity     = 10
+  #     max_capacity     = 5
   #     min_capacity     = 3
   #     additional_tags = {
   #       name = "Roi-test"
@@ -22,5 +27,16 @@ module "roi-eks" {
       
   #   }
   # }
-
+    
+    node_groups_defaults = {
+    additional_tags	=var.cluster_tags
+    max_capacity= 5
+    min_capacity=3
+    desired_capacity=3
+    instance_types=["t3.large"]
+    
+    
+  }
+  node_groups = {
+}
 }
--- node_group.tf
+++ node_group.tf
@@ -1,6 +1,14 @@
-module "my-node-group" {
-  source = "terraform-aws-modules/eks/aws//modules/node_groups"
-  ng_depends_on = aws_iam_role.eks-node-group-role
-  default_iam_role_arn = aws_iam_role.eks-node-group-role.arn
-  cluster_name = var.cluster_name
-}
+# module "my-node-group" {
+#   source = "terraform-aws-modules/eks/aws//modules/node_groups"
+#   ng_depends_on = aws_iam_role.eks-node-group-role
+#   default_iam_role_arn = aws_iam_role.eks-node-group-role.arn
+#   cluster_name = var.cluster_name
+  
+#   node_groups_defaults = {
+#     additional_tags	=var.cluster_tags
+#     max_capacity= 5
+#     min_capacity=3
+#     desired_capacity=3
+#     instance_types=["t3.medium"]
+#   }
+# }
