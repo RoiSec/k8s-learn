20ac3fcbf5577296d5a0ef11fcf3b04fa74f0329
RoiSec <roi>
2021-11-06 15:21:54 +0200

    edit node group

--- node_group.tf
+++ node_group.tf
@@ -2,5 +2,5 @@ module "my-node-group" {
   source = "terraform-aws-modules/eks/aws//modules/node_groups"
   ng_depends_on = aws_iam_role.eks-node-group-role
   default_iam_role_arn = aws_iam_role.eks-node-group-role.arn
-  
+  cluster_name = var.cluster_name
 }
