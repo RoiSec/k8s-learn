051b302c870f3d1a213b44c56928be9900237a5e
RoiSec <roi>
2021-11-06 14:29:35 +0200

    delete k8s provider

--- main.tf
+++ main.tf
@@ -1,19 +1,6 @@
 provider "aws" {
   region = "eu-west-2"
 }
-data "aws_eks_cluster" "eks" {
-  name = module.roi-eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "eks" {
-  name = module.roi-eks.cluster_id
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
 module "roi-eks" {
   source          = "terraform-aws-modules/eks/aws"
   
